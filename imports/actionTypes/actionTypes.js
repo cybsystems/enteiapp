53a81deeefc209052af6a0a49caefc52b6d2e797
Amit <amit.waghamare@liveheath.in>
2019-07-14 19:50:36 +0530

    MINOR changes

--- imports/actionTypes/actionTypes.js
+++ imports/actionTypes/actionTypes.js
@@ -1,5 +1,5 @@
 export const ON_SELECT_CATEGORY = 'ON_SELECT_CATEGORY'
-export const ON_GET_VIDEOS = 'ON_GET_VIDEO'
+export const ON_GET_VIDEOS = 'ON_GET_VIDEOS'
 export const ON_STORE_VIDEO = 'ON_STORE_VIDEO'
 export const ON_IS_LOGIN = 'ON_IS_LOGIN'
 export const ON_LOGIN = 'ON_LOGIN'
